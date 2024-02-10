2d48b7ca5f5eec96f6f53c9bc94b7151e84f5a79
WeijiaLinaSun <ws2406@nyu.edu>
2024-02-10 00:36:37 -0500

    finished this assignment!

--- analyze.py
+++ analyze.py
@@ -1,1 +1,50 @@
-# Place code below to do the analysis part of the assignment.
+
+import csv
+
+def an():
+
+    # opens up your cleaned up data file, data/clean_data.csv and imports it using Python's csv module.
+    # outputs the average temperature anomaly in degrees Farenheit for each decade since 1880.
+    # For example, output the average temperature anomaly for the decades:
+    # 1880 to 1889
+    # 1890 to 1899
+    # 1900 to 1909
+    # ...and so on.
+    average_temperature = []
+    with open('data/clean_data.csv') as file:
+       
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        
+        for row in csv_reader:
+            t = 0
+            for i in range(1,13):
+                t += float(row[i+1])
+            average_temperature.append(t/12)
+
+    count = 0
+    m = 0
+    for i in range(0, len(average_temperature)):
+
+        m += average_temperature[i]
+        count +=1
+        if count % 10 == 0:
+            m /= 10
+            print(f'{1870 + count} to {1879 + count}: {format(m, ".1f")}')
+            m = 0
+
+        if 1880 + count == 2020:
+
+            for j in range(i+1, len(average_temperature)):
+
+                m += average_temperature[j]
+            m /= (len(average_temperature) - count)
+
+            print(f'{1880 + count} to {1880 +len(average_temperature)-1 }: {format(m, ".1f")}')
+
+
+
+
+
+
+an()
--- munge.py
+++ munge.py
@@ -1,1 +1,59 @@
 # Place code below to do the munging part of this assignment.
+import re
+
+def ana():
+    # You must write the code using plain Python.
+    # Issues your program must address:
+    # read the data directly from the file 66/data/GLB.Ts+dSST.txt
+    #
+
+    with open('data/GLB.Ts+dSST.txt') as f:
+        data = f.readlines()
+
+    
+    data = data[7:-6]
+    
+    data = [x for x in data if x.strip() != '']
+
+    # the column headings are repeated on multiple lines throughout the file -
+    # remove all but the first line of column headings.
+    for i, e in enumerate(data):
+        if e.startswith('Year') and i != 0:
+            
+            data.pop(i)
+
+    # there is missing data indicated with *** figure out how to handle missing data so that your analyses are correct.
+    
+    for i, e in enumerate(data):
+        
+        data[i] = re.sub(r'\s+', ',', e)
+        data[i] = data[i].replace('****', '0').replace('***', '0')
+        data[i] = data[i].split(',')[:-2]
+
+    data.pop(-1)
+
+
+    # the temperature anomalies in this data are given in 0.01 degrees Celsius.
+    # Convert temperature anomalies to Farenheit, the US standard unit of temperature:
+    # the formula to do this can be found within the data set
+    # format the results so that there's one decimal place (use format with .1f as the second argument)
+    # since this data is in fixed-width column format,
+    # there are inconsistent numbers of spaces separating the numeric values
+    for i, e in enumerate(data[1:]):
+        for j, e1 in enumerate(e[1:]):
+            data[i+1][j+1 ] = float(e1) * 0.01 * 9 / 5
+            data[i+1][j+1 ]= format(data[i+1][j+1 ], '.1f')
+
+    
+    with open('data/clean_data.csv', 'w') as f:
+        for e in data:
+            f.write(','.join(e) + '\n') 
+
+
+
+
+
+
+
+
+ana()
